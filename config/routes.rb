18939b5736a49339014d6a2ea63b5fe274c87bb9
bl2i4n <bl2i4n@yahoo.com>
2017-05-02 00:01:09 -0700

    was able to get view with sign in working but confirmation email not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  root 'welcome#index'
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
