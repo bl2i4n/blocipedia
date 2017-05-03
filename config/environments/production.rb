f00fe953a1707c1173f10642d22af4dad0625ec2
bl2i4n <bl2i4n@yahoo.com>
2017-05-02 23:49:08 -0700

    added sendgrid info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,11 +78,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'desolate-sierra-58992.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'desolate-sierra-58992.herokuapp.com' }
 
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
-      :port           => '587',
+      :port           => '2525',
       :authentication => :plain,
       :user_name      => ENV['SENDGRID_USERNAME'],
       :password       => ENV['SENDGRID_PASSWORD'],
