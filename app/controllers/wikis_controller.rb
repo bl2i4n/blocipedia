5c31dc1b665669eb1ec64513010e83b584894a50
bl2i4n <bl2i4n@yahoo.com>
2017-05-06 11:51:38 -0700

    added CRUD functionality

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -4,11 +4,56 @@ class WikisController < ApplicationController
   end
 
   def show
+    @wiki = Wiki.find(params[:id])
   end
 
   def new
+    @wiki = Wiki.new
   end
 
-  def edit
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash.now[:alert]= "There was an error saving the wiki. Please try again."
+      render :new
+    end
   end
+
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+
+    if @wiki.save
+      flash[:notice] = "Wiki was updated"
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was error saving wiki. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+     @wiki = Wiki.find(params[:id])
+
+     if @wiki.destroy
+       flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+       redirect_to wikis_path
+     else
+       flash.now[:alert] = "There was an error deleting the post."
+       render :show
+     end
+   end
+
 end
